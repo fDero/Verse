38abcc3d790905447ad7711319614108e7bd8b90
fDero <francescodero@outlook.it>
2023-11-08 19:58:49 +0100

    better scoping mechanism

--- src/interpreter/execute_statement.cpp
+++ src/interpreter/execute_statement.cpp
@@ -2,14 +2,33 @@
 #include "../../prototypes/procedures.hpp"
 
 
+void update_local_symbols(const Instruction& instruction, std::vector<Identifier>& local_symbols){
+    if (std::holds_alternative<Variable>(instruction)){
+        local_symbols.push_back(Identifier{std::get<Variable>(instruction).name});
+    }
+    else if (std::holds_alternative<Constant>(instruction)){
+        local_symbols.push_back(Identifier{std::get<Constant>(instruction).name});
+    }
+}
+
+void remove_local_symbols_from_outside_context(const std::vector<Identifier>& local_symbols, ExecutionContext& context){
+    for (const Identifier& local : local_symbols){
+        context.constant_types.erase(local.name);
+        context.constant_values.erase(local.name);
+        context.variable_types.erase(local.name);
+        context.variable_values.erase(local.name);
+    }
+}
+
 Instruction execute_conditional(const Conditional& conditional, ExecutionContext& context){
     auto [condition_type, condition_value] = execute_expression(*conditional.condition, context);
+    std::vector<Identifier> local_symbols;
     if (not std::holds_alternative<BaseType>(condition_type)) throw std::runtime_error{ "not a valid if condition" };
     if (std::get<BaseType>(condition_type).base_type != "Bool") throw std::runtime_error{ "not a valid if condition" };
-    if (not std::holds_alternative<BoolValue>(condition_value)) throw InternalCompilerError { "teschio" };
     bool boolean_condition = std::get<BoolValue>(condition_value);
     const std::vector<Instruction>& execution_path = (boolean_condition)? conditional.then : conditional.otherwise;
     for (const Instruction& instruction : execution_path){
+        update_local_symbols(instruction, local_symbols);
         Instruction callback = execute_instruction(instruction, context);
         if (not std::holds_alternative<DoNothing>(callback)){
             if (std::holds_alternative<Return>(callback))    return callback;
@@ -18,33 +37,41 @@ Instruction execute_conditional(const Conditional& conditional, ExecutionContext
             throw InternalCompilerError { "cannot recognize callback instruction" };
         }
     }
+    remove_local_symbols_from_outside_context(local_symbols, context);
     return DoNothing{};
 }
 
 Instruction execute_while_loop(const WhileLoop& while_loop, ExecutionContext& context){
     while(true){
         auto [condition_type, condition_value] = execute_expression(*while_loop.condition, context);
-        if (not std::holds_alternative<BaseType>(condition_type)) throw std::runtime_error{ "not a valid if condition" };
-        if (std::get<BaseType>(condition_type).base_type != "Bool") throw std::runtime_error{ "not a valid if condition" };
-        if (not std::holds_alternative<BoolValue>(condition_value)) throw InternalCompilerError { "teschio" };
+        if (not std::holds_alternative<BaseType>(condition_type)) throw std::runtime_error{ "not a valid condition" };
+        if (std::get<BaseType>(condition_type).base_type != "Bool") throw std::runtime_error{ "not a valid condition" };
         if (not std::get<BoolValue>(condition_value)) return DoNothing{};
+        std::vector<Identifier> local_symbols;
         for (const Instruction& instruction : while_loop.code){
+            update_local_symbols(instruction, local_symbols);
             Instruction callback = execute_instruction(instruction, context);
             if (not std::holds_alternative<DoNothing>(callback)){
+                remove_local_symbols_from_outside_context(local_symbols, context);
                 if (std::holds_alternative<Return>(callback))    return DoNothing{};
                 if (std::holds_alternative<Break>(callback))     return DoNothing{};
                 if (std::holds_alternative<Continue>(callback))  break;
                 throw InternalCompilerError { "cannot recognize callback instruction" };
             }
         }
+        remove_local_symbols_from_outside_context(local_symbols, context);
     }
+    return DoNothing{};
 }
 
 Instruction execute_until_loop(const UntilLoop& until_loop, ExecutionContext& context){
     while(true){
+        std::vector<Identifier> local_symbols;
         for (const Instruction& instruction : until_loop.code){
+            update_local_symbols(instruction, local_symbols);
             Instruction callback = execute_instruction(instruction, context);
             if (not std::holds_alternative<DoNothing>(callback)){
+                remove_local_symbols_from_outside_context(local_symbols, context);
                 if (std::holds_alternative<Return>(callback))    return DoNothing{};
                 if (std::holds_alternative<Break>(callback))     return DoNothing{};
                 if (std::holds_alternative<Continue>(callback))  break;
@@ -52,9 +79,10 @@ Instruction execute_until_loop(const UntilLoop& until_loop, ExecutionContext& co
             }
         }
         auto [condition_type, condition_value] = execute_expression(*until_loop.condition, context);
-        if (not std::holds_alternative<BaseType>(condition_type)) throw std::runtime_error{ "not a valid if condition" };
-        if (std::get<BaseType>(condition_type).base_type != "Bool") throw std::runtime_error{ "not a valid if condition" };
-        if (not std::holds_alternative<BoolValue>(condition_value)) throw InternalCompilerError { "teschio" };
+        remove_local_symbols_from_outside_context(local_symbols, context);
+        if (not std::holds_alternative<BaseType>(condition_type)) throw std::runtime_error{ "not a valid condition" };
+        if (std::get<BaseType>(condition_type).base_type != "Bool") throw std::runtime_error{ "not a valid condition" };
         if (std::get<BoolValue>(condition_value)) return DoNothing{};
     }
+    return DoNothing{};
 }
