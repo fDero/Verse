924ee3044ae8d269464a9fef598c202156c2317d
fDero <francescodero@outlook.it>
2023-11-08 22:55:02 +0100

    minor refactoring

--- src/typesystem/generic_type_utils.cpp
+++ src/typesystem/generic_type_utils.cpp
@@ -1,39 +1,71 @@
 #include "../../include/verse.hpp"
 #include "../../prototypes/procedures.hpp"
 
+TypeSignature apply_generics_to_nested_type(const NestedType& nested_type, const GenericsLookupTable& generics_lookup_table){
+    const TypeSignature& left_type = *nested_type.left;
+    const TypeSignature& right_type = *nested_type.right;
+    TypeSignature instanciated_left_type = apply_generics_to_typesignature(left_type, generics_lookup_table);
+    TypeSignature instanciated_right_type = apply_generics_to_typesignature(right_type, generics_lookup_table);
+    std::shared_ptr<TypeSignature> left_type_ptr = std::make_shared<TypeSignature>(instanciated_left_type);
+    std::shared_ptr<TypeSignature> right_type_ptr = std::make_shared<TypeSignature>(instanciated_right_type);
+    return NestedType { left_type_ptr, right_type_ptr };
+}
+
+TypeSignature apply_generics_to_array_type(const Array& array_type, const GenericsLookupTable& generics_lookup_table){
+    const TypeSignature& stored_type = *array_type.type;
+    int length = array_type.length;
+    TypeSignature instanciated_stored_type = apply_generics_to_typesignature(stored_type, generics_lookup_table);
+    std::shared_ptr<TypeSignature> new_stored_type = std::make_shared<TypeSignature>(instanciated_stored_type);
+    return Array { new_stored_type, length };   
+}
+
+TypeSignature apply_generics_to_pointer_type(const Pointer& pointer_type, const GenericsLookupTable& generics_lookup_table){
+    const TypeSignature& pointed = *pointer_type.pointed;
+    TypeSignature instanciated_pointed_type = apply_generics_to_typesignature(pointed, generics_lookup_table);
+    std::shared_ptr<TypeSignature> new_pointed_type = std::make_shared<TypeSignature>(instanciated_pointed_type);
+    return Pointer { new_pointed_type };
+}
+
+TypeSignature apply_generics_to_base_type_deprecated(const TypeSignature& base_type, const GenericsLookupTable& generics_lookup_table){
+    for (const auto& pair : generics_lookup_table){
+        if (std::get<BaseType>(base_type).base_type == pair.first) return pair.second;
+    }
+    if (std::get<BaseType>(base_type).generics.empty()) return base_type;
+    TypeSignature instanciated_generic_type = base_type;
+    for (TypeSignature& generic : std::get<BaseType>(instanciated_generic_type).generics){
+        generic = apply_generics_to_typesignature(generic, generics_lookup_table);
+    }
+    return instanciated_generic_type;
+}
+
+TypeSignature apply_generics_to_base_type(const BaseType& type, const GenericsLookupTable& generics_lookup_table){
+    BaseType instanciated_type = type;
+    auto base_type_replacement = generics_lookup_table.find(type.base_type);
+    if (base_type_replacement != generics_lookup_table.end()) return base_type_replacement->second;   
+    for (TypeSignature& generic : instanciated_type.generics){
+        generic = apply_generics_to_typesignature(generic, generics_lookup_table);
+    }
+    return instanciated_type;
+}
+
 TypeSignature apply_generics_to_typesignature(const TypeSignature& generic_type, const GenericsLookupTable& generics_lookup_table){
     if (std::holds_alternative<BaseType>(generic_type)){
-        for (const auto& pair : generics_lookup_table){
-            if (std::get<BaseType>(generic_type).base_type == pair.first) return pair.second;
-        }
-        if (std::get<BaseType>(generic_type).generics.empty()) return generic_type;
-        TypeSignature instanciated_generic_type = generic_type;
-        for (TypeSignature& generic : std::get<BaseType>(instanciated_generic_type).generics){
-            generic = apply_generics_to_typesignature(generic, generics_lookup_table);
-        }
-        return instanciated_generic_type;
+        BaseType generic_base_type = std::get<BaseType>(generic_type);
+        return apply_generics_to_base_type(generic_base_type, generics_lookup_table);
     }
     else if (std::holds_alternative<Pointer>(generic_type)){
-        const TypeSignature& pointed = *std::get<Pointer>(generic_type).pointed;
-        TypeSignature instanciated_pointed_type = apply_generics_to_typesignature(pointed, generics_lookup_table);
-        std::shared_ptr<TypeSignature> new_pointed_type = std::make_shared<TypeSignature>(instanciated_pointed_type);
-        return Pointer { new_pointed_type };
+        Pointer generic_pointer_type = std::get<Pointer>(generic_type);
+        return apply_generics_to_pointer_type(generic_pointer_type, generics_lookup_table);
     }
     else if (std::holds_alternative<Array>(generic_type)){
-        const TypeSignature& array_type = *std::get<Array>(generic_type).type;
-        int length = std::get<Array>(generic_type).length;
-        TypeSignature instanciated_array_type = apply_generics_to_typesignature(array_type, generics_lookup_table);
-        std::shared_ptr<TypeSignature> new_array_type = std::make_shared<TypeSignature>(instanciated_array_type);
-        return Array { new_array_type, length };
+        Array generic_array_type = std::get<Array>(generic_type);
+        return apply_generics_to_array_type(generic_array_type, generics_lookup_table);
     }
     else if (std::holds_alternative<NestedType>(generic_type)){
-        const TypeSignature& left_type = *std::get<NestedType>(generic_type).left;
-        const TypeSignature& right_type = *std::get<NestedType>(generic_type).right;
-        TypeSignature instanciated_left_type = apply_generics_to_typesignature(left_type, generics_lookup_table);
-        TypeSignature instanciated_right_type = apply_generics_to_typesignature(right_type, generics_lookup_table);
-        std::shared_ptr<TypeSignature> left_type_ptr = std::make_shared<TypeSignature>(instanciated_left_type);
-        std::shared_ptr<TypeSignature> right_type_ptr = std::make_shared<TypeSignature>(instanciated_right_type);
-        return NestedType { left_type_ptr, right_type_ptr };
+        NestedType generic_nested_type = std::get<NestedType>(generic_type);
+        return apply_generics_to_nested_type(generic_nested_type, generics_lookup_table);
     }
-    throw InternalCompilerError { "somehow a typesignature managed to hold something that is not a base-type, nor a pointer nor an array" };
+    throw InternalCompilerError { 
+        "somehow a typesignature managed to hold something that is not a base-type, nor a pointer nor an array" 
+    };
 }
